8124295cd70cb0a556dcefd4af8e9584e23d04ce
Akash Das <mfs.akash@gmail.com>
2022-01-17 18:33:02 +0530

    Recursion

---
+++ recursion/bound_recursion.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Function calling itself recursively
+ *
+ * @return void
+ */
+function recursive_func()
+{
+    static $counter = 0;
+    $counter++;
+    print("Recursive function call number " . $counter . "\n");
+
+    if ($counter < 3) {
+        recursive_func();
+    }
+
+    print("Bye from recursive_func, call number " . $counter . "\n");
+    $counter--;
+}
+
+// Main function call
+print("Execution of main function started\n");
+recursive_func();
+print("Execution of main function over\n");
